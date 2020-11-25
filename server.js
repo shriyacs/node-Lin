9de2eba82a20eae72ac3cc22f61d1057a36f6a0e
shriya <shriya.cs13@gmail.com>
2020-11-25 19:57:49 +0530

    Initial with static design

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname))
+var server = app.listen(3000, () => {
+    console.log('Server is listening at port: ', server.address().port)
+})
