ebdf7e01d3a36d16d3437cd6cc02cbf7dd91d04f
shriya <shriya.cs13@gmail.com>
2020-11-27 20:07:25 +0530

    create a post message service

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 var messages = [
     {name: 'Shriya', message: 'Hey'},
@@ -12,6 +16,11 @@ var messages = [
 
 app.get('/messages', (req,res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req,res) => {
+    messages.push(req.body)
+    res.sendStatus(200) // 200 code - everything is ok
 })
 
 var server = app.listen(3000, () => {
