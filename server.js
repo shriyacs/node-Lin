83bb0a0860569b5bafce05747c43df30d8eb2755
shriya <shriya.cs13@gmail.com>
2020-12-02 20:39:34 +0530

    using mongoose and mongodb(mlab) to send messages from database

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose') //userpassword
 
 app.use(express.static(__dirname))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+var dbUrl = 'mongodb+srv://user:userpassword@node-lin.mnmxa.mongodb.net/test'
+var dbURL = 'mongodb+srv://user:userpassword@node-lin.mnmxa.mongodb.net/nodeTempBase?retryWrites=true&w=majority'
+
+var Message = mongoose.model('Message', {
+    name: 'string',
+    message: 'string'
+})
 
 var messages = [
     {name: 'Shriya', message: 'Hey'},
@@ -17,17 +25,28 @@ var messages = [
 ]
 
 app.get('/messages', (req,res) => {
-    res.send(messages)
+    Message.find({}, (err,messages)=>{
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req,res) => {
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200) // 200 code - everything is ok
+    var message = new Message(req.body)
+    message.save((err)=> {
+        if(err)
+            sendStatus(500)
+        messages.push(req.body)
+        io.emit('message', req.body)
+        res.sendStatus(200) // 200 code - everything is ok
+    })
 })
 
 io.on('connection', (socket)=> {
     console.log('User Connected')
+})
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true } ,(err)=> {
+    console.log('mongo db connection successful. err =', err)
 })
 
 var server = http.listen(3000, () => {
